255f698b30c3e0795dda569583a109907d7d4bbd
Rustam <tazyrov@skbkontur.ru>
2026-08-23 19:42:26 +0500

    App version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2.0"
+        versionName = "1.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
